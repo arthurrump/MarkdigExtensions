56db5b4d4fbfebec8e718d4722502dd4f67c4864
Arthur Rump <hello@arthurrump.com>
2019-04-27 21:29:39 +0200

    Add more UrlRewriter tests

--- test/Markdig.Extensions.Tests/UrlRewriterTests.fs
+++ test/Markdig.Extensions.Tests/UrlRewriterTests.fs
@@ -4,7 +4,6 @@ open Expecto
 
 open Markdig
 open Markdig.Extensions.UrlRewriter
-open Markdig.Syntax.Inlines
 
 [<Tests>]
 let tests =
@@ -23,5 +22,33 @@ let tests =
             Expect.isTrue (html.Contains("https://example.net")) "Contains new https"
             Expect.isTrue (html.Contains("http://example.com")) "Not replaced http"
             Expect.isFalse (html.Contains("https://example.com")) "Replaced old https"
+        }
+
+        test "Rewrites only images" {
+            let pipeline =
+                MarkdownPipelineBuilder()
+                    .UseUrlRewriter(fun link ->
+                        if link.IsImage
+                        then "http://newImageUrl.net"
+                        else link.Url)
+                    .Build()
+
+            let markdown = "[Test1](https://example.com), ![Image](http://example.net)"
+            let html = Markdown.ToHtml(markdown, pipeline)
+            Expect.isTrue (html.Contains("https://example.com")) "Not replaced anchor link"
+            Expect.isTrue (html.Contains("http://newImageUrl.net")) "Contains new image url"
+            Expect.isFalse (html.Contains("http://example.net")) "Replaced old image url"
+        }
+
+        test "No rewrite" {
+            let pipeline =
+                MarkdownPipelineBuilder()
+                    .UseUrlRewriter(fun link -> link.Url)
+                    .Build()
+
+            let markdown = "[Test1](https://example.com), ![Image](http://example.net)"
+            let expected = """<p><a href="https://example.com">Test1</a>, <img src="http://example.net" alt="Image" /></p>""" + "\n"
+            let html = Markdown.ToHtml(markdown, pipeline)
+            Expect.equal html expected "Correct HTML"
         }
     ]
